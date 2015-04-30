3c958eb97a30ca0dac025a56e0e08e53edc6e93e
Alex <abartlett@college.harvard.edu>
2015-04-30 12:17:28 -0400

    working on readinfile

--- readinfile.c
+++ readinfile.c
@@ -1,8 +1,33 @@
 #include "autocorrectTrain.c"
 
 // changes txt file to list of strings, removes punctuation, makes lowercase
-char** filetostrs (FILE* file) {
-
+char** filetostrs (const char* filename, int numwords) {
+	FILE* fp = fopen(filename, 'r');
+	if (fp == NULL) return NULL;
+	char* currentword = malloc(100 * sizeof(char));
+	fgets(currentword, 100, fp);
+	char** strlist = malloc(numwords*100*sizeof(char));
+	int index = 0;
+	while (currentword != NULL)
+	{
+		int wordlen = strlen(currentword);
+		for (int i = 0; i < wordlen; i++)
+		{
+			currentword[i] = tolower(currentword[i]);
+			if !(isalpha(currentword[i]))
+			{
+				strncpy(firsthalf, currentword, i-1);
+				strncpy(secondhalf, currentword[i+1], wordlen - i - 1);
+				currentword = strcat(firsthalf, secondhalf);
+				wordlen--;
+			}
+		}
+		strlist[index] = currentword;
+		index++;
+		fgets(currentword, 100, fp);
+	}
+	fclose(fp);
+	return strlist;
 }
 
 // takes a list of incorrectly spelled words and corrects them, outputting 
