847e207e6f3e900837ab473410040d2a0e8258ab
Alex <abartlett@college.harvard.edu>
2015-04-30 12:31:51 -0400

    filetostr compiles

--- readinfile.c
+++ readinfile.c
@@ -1,8 +1,9 @@
 #include "autocorrectTrain.c"
+#include <ctype.h>
 
 // changes txt file to list of strings, removes punctuation, makes lowercase
 char** filetostrs (const char* filename, int numwords) {
-	FILE* fp = fopen(filename, 'r');
+	FILE* fp = fopen(filename, "r");
 	if (fp == NULL) return NULL;
 	char* currentword = malloc(100 * sizeof(char));
 	fgets(currentword, 100, fp);
@@ -14,10 +15,12 @@ char** filetostrs (const char* filename, int numwords) {
 		for (int i = 0; i < wordlen; i++)
 		{
 			currentword[i] = tolower(currentword[i]);
-			if !(isalpha(currentword[i]))
+			if (!(isalpha(currentword[i])))
 			{
+				char* firsthalf = malloc(i*sizeof(char));
+				char* secondhalf = malloc((wordlen -i) * sizeof(char));
 				strncpy(firsthalf, currentword, i-1);
-				strncpy(secondhalf, currentword[i+1], wordlen - i - 1);
+				strncpy(secondhalf, currentword+(i+1), wordlen - i - 1);
 				currentword = strcat(firsthalf, secondhalf);
 				wordlen--;
 			}
@@ -33,11 +36,11 @@ char** filetostrs (const char* filename, int numwords) {
 // takes a list of incorrectly spelled words and corrects them, outputting 
 // a list of corrected words
 char** correctFile (char** incorrect) {
-
+	return NULL;
 }
 
 // compares the corrected list of words to the list of incorrected words, returns
 // integer number of disagreements
 int compareFiles (char** correct, char** incorrect) {
-
+	return 0;
 }
