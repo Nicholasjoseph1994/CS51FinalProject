5f9d428d4aa55939b9a6597bc4511da1d2cfd371
Alex <abartlett@college.harvard.edu>
2015-04-30 11:40:08 -0400

    new signatures for file reading

---
+++ readinfile.c
@@ -0,0 +1,18 @@
+#include "autocorrectTrain.c"
+
+// changes txt file to list of strings, removes punctuation, makes lowercase
+char** filetostrs (FILE* file) {
+
+}
+
+// takes a list of incorrectly spelled words and corrects them, outputting 
+// a list of corrected words
+char** correctFile (char** incorrect) {
+
+}
+
+// compares the corrected list of words to the list of incorrected words, returns
+// integer number of disagreements
+int compareFiles (char** correct, char** incorrect) {
+
+}
